6694e912d1458dace47ffd44c244d9a12ff9cb02
Aayush Sanghvi <sanghvi.aay@northeastern.edu>
2023-10-23 12:55:59 +0000

    LAB2 updated

--- LAB2/src/gpsrtk_driver/python/gpsrtk.py
+++ LAB2/src/gpsrtk_driver/python/gpsrtk.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
             if line1[2] == '':
                 rospy.logwarn("No data")
             else:
-                if line.startswith("b'$GPGGA"):
+                if line.startswith("b'$GNGGA"):
                     
                     
                     timestamp=str(float(line1[1]))
